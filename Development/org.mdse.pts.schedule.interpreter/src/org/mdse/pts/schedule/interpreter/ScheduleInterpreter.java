7f260b305f69b24f4c79bde0125483804c2edab0
matt035343 <matt035343@gmail.com>
2020-05-02 17:21:57 +0200

    A bit clean up for #9

--- Development/org.mdse.pts.schedule.interpreter/src/org/mdse/pts/schedule/interpreter/ScheduleInterpreter.java
+++ Development/org.mdse.pts.schedule.interpreter/src/org/mdse/pts/schedule/interpreter/ScheduleInterpreter.java
@@ -13,6 +13,7 @@ import org.mdse.pts.timetable.TimetableFactory;
 
 import common.CommonFactory;
 import common.Time;
+import common.WeekDay;
 
 import org.mdse.pts.network.Leg;
 import org.mdse.pts.network.Network;
@@ -29,18 +30,14 @@ import org.eclipse.emf.common.util.EList;
 public class ScheduleInterpreter {
 	public HashMap<String,Timetable> interpret(Schedule schedule) {
 		EList<Station> stations = schedule.getNetwork().getStations();
-		HashMap<String,Timetable> timetables = new HashMap<>();
-		
-		/*Instantiate timetables*/
-		for(int i = 0; i < stations.size(); i++) {
-			Station station = stations.get(i);
-			Timetable timetable = TimetableFactory.eINSTANCE.createTimetable();
-			timetable.setStation(station.getName());
-			timetables.put(station.getName(), timetable);
-		}
-		
+		HashMap<String,Timetable> timetables = createTimetables(stations);
 		EList<TrainSchedule> trainSchedules = schedule.getTrainschedules();
+		interpretTrainSchedules(trainSchedules, stations, timetables);
 		
+		return timetables;
+	}
+	
+	private void interpretTrainSchedules(EList<TrainSchedule> trainSchedules, EList<Station> stations, HashMap<String, Timetable> timetables) {
 		for(TrainSchedule trainSchedule : trainSchedules) {
 			EList<Frequency> frequencies = trainSchedule.getFrequency();
 			EList<Stop> routeStops = trainSchedule.getRoute().getStop();
@@ -53,39 +50,17 @@ public class ScheduleInterpreter {
 						for(int i = 0; i < routeStops.size(); i++) {
 							Stop stop = routeStops.get(i);
 							
-							//Find next station
 							Station nextStation = null;
 							if(i < routeStops.size()-1) nextStation = stop.getStation();
 							Station station = stop.getStation();
 							
-							//Calculate run time
-							int distance = 0;
-							Leg via = stop.getVia();
-							List<Leg> legsToStop = getLegsEndingAtStation(station);
-							if(via != null) distance = via.getDistance();
-							else if(legsToStop.size() >= 1) distance = legsToStop.get(0).getDistance(); //if multiple legs and no via, just take the first
-							else throw new ScheduleInterpreterException("No legs goes to this station! " + station.getName());
-							int minutes = distance / 2; //TODO: train.getSpeed()/60; //(kilometers per minute)
+							int minutes = getRunTimeInMinutes(stop.getVia(), station);
 							
-							//Create arrival
 							currentTime = addMinutesToTime(currentTime, minutes);
-							Arrival arrival = TimetableFactory.eINSTANCE.createArrival();
-							arrival.setPlatform(stop.getPlatform());
-							arrival.setPreviousStation(prevStation != null ? prevStation.getName() : null);
-							arrival.setTime(currentTime);
-							arrival.setWeekDay(day.getWeekday());
-							arrival.setTrain(train.getName());
-							timetables.get(station.getName()).getArrivals().add(arrival);
+							addArrival(timetables, station, prevStation, currentTime, day.getWeekday(), train, stop.getPlatform());
 							
-							//Create departure
 							currentTime = addMinutesToTime(currentTime, stop.getDuration());
-							Departure departure = TimetableFactory.eINSTANCE.createDeparture();
-							departure.setPlatform(stop.getPlatform());
-							departure.setNextStation(nextStation != null ? nextStation.getName() : null);
-							departure.setTime(currentTime);
-							departure.setWeekDay(day.getWeekday());
-							departure.setTrain(train.getName());
-							timetables.get(station.getName()).getDepartures().add(departure);
+							addDeparture(timetables, station, nextStation, currentTime, day.getWeekday(), train, stop.getPlatform());
 							
 							prevStation = station;
 						}
@@ -95,7 +70,46 @@ public class ScheduleInterpreter {
 			}
 			
 		}
-		
+	}
+	
+	private int getRunTimeInMinutes(Leg via, Station station) {
+		int distance = 0;
+		List<Leg> legsToStop = getLegsEndingAtStation(station);
+		if(via != null) distance = via.getDistance();
+		else if(legsToStop.size() >= 1) distance = legsToStop.get(0).getDistance(); //if multiple legs and no via, just take the first
+		else throw new ScheduleInterpreterException("No legs goes to this station! " + station.getName());
+		int minutes = distance / 2; //TODO: train.getSpeed()/60; //(kilometers per minute)
+		return minutes;
+	}
+	
+	private void addDeparture(HashMap<String, Timetable> timetables, Station station, Station nextStation, Time currentTime, WeekDay day, Train train, int platform) {
+		Departure departure = TimetableFactory.eINSTANCE.createDeparture();
+		departure.setPlatform(platform);
+		departure.setNextStation(nextStation != null ? nextStation.getName() : null);
+		departure.setTime(currentTime);
+		departure.setWeekDay(day);
+		departure.setTrain(train.getName());
+		timetables.get(station.getName()).getDepartures().add(departure);
+	}
+
+	private void addArrival(HashMap<String,Timetable> timetables, Station station, Station prevStation, Time currentTime, WeekDay day, Train train, int platform) {
+		Arrival arrival = TimetableFactory.eINSTANCE.createArrival();
+		arrival.setPlatform(platform);
+		arrival.setPreviousStation(prevStation != null ? prevStation.getName() : null);
+		arrival.setTime(currentTime);
+		arrival.setWeekDay(day);
+		arrival.setTrain(train.getName());
+		timetables.get(station.getName()).getArrivals().add(arrival);
+	}
+
+	private HashMap<String,Timetable> createTimetables(EList<Station> stations) {
+		HashMap<String,Timetable> timetables = new HashMap<>();
+		for(int i = 0; i < stations.size(); i++) {
+			Station station = stations.get(i);
+			Timetable timetable = TimetableFactory.eINSTANCE.createTimetable();
+			timetable.setStation(station.getName());
+			timetables.put(station.getName(), timetable);
+		}
 		return timetables;
 	}
 	
