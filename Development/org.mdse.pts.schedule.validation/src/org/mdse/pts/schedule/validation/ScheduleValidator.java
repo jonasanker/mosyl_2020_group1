722a429173888aae003cb4546d45fb0c24aff728
Jonas Anker Rasmussen <jonasanker@gmail.com>
2020-05-12 12:41:23 +0200

    ScheduleValidator base project #7

---
+++ Development/org.mdse.pts.schedule.validation/src/org/mdse/pts/schedule/validation/ScheduleValidator.java
@@ -0,0 +1,29 @@
+package org.mdse.pts.schedule.validation;
+
+import java.util.Map;
+
+import org.eclipse.emf.common.util.DiagnosticChain;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EValidator;
+import org.eclipse.emf.ecore.util.EObjectValidator;
+import org.eclipse.ui.IStartup;
+import org.mdse.pts.schedule.SchedulePackage;
+
+public class ScheduleValidator extends EObjectValidator implements IStartup {
+
+	@Override
+	public void earlyStartup() {
+		EValidator.Registry.INSTANCE.put(SchedulePackage.eINSTANCE, new ScheduleValidator());
+	}
+
+	@Override
+	public boolean validate(EObject eObject, DiagnosticChain diagnostics, Map<Object, Object> context) {
+		return validate(eObject.eClass(), eObject, diagnostics, context);
+	}
+
+	@Override
+	public boolean validate(EClass eClass, EObject eObject, DiagnosticChain diagnostics, Map<Object, Object> context) {
+		return false;
+	}
+}
